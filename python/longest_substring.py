2c31e0a4d4e6434fc6c6dbbc266ac7c495ce4e33
Ewooral <boahen.cos@gmail.com>
2022-02-10 04:05:37 +0000

    longest substring without repeating characters

--- python/longest_substring.py
+++ python/longest_substring.py
@@ -25,4 +25,44 @@ Constraints:
 
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
+
+
+APPROACH ONE - BRUTE FORCE
+Intuition
+
+Check all the substring one by one to see if it has no duplicate character.
+
+Algorithm
+
+Suppose we have a function boolean allUnique(String substring) which will return true if the characters in the substring are all unique, otherwise false. We can iterate through all the possible substrings of the given string s and call the function allUnique. If it turns out to be true, then we update our answer of the maximum length of substring without duplicate characters.
+
+Now let's fill the missing parts:
+
+To enumerate all substrings of a given string, we enumerate the start and end indices of them. Suppose the start and end indices are ii and jj, respectively. Then we have 0 \leq i \lt j \leq n0≤i<j≤n (here end index jj is exclusive by convention). Thus, using two nested loops with ii from 0 to n - 1n−1 and jj from i+1i+1 to nn, we can enumerate all the substrings of s.
+
+To check if one string has duplicate characters, we can use a set. We iterate through all the characters in the string and put them into the set one by one. Before putting one character, we check if the set already contains it. If so, we return false. After the loop, we return true.
+
+Time complexity : O(n^3)O(n3).
+Space complexity : O(min(n, m))O(min(n,m)). We need O(k)O(k) space for checking a substring has no duplicate characters, where kk is the size of the Set. The size of the Set is upper bounded by the size of the string nn and the size of the charset/alphabet mm.
 """
+
+
+class Solution:
+    def length_of_longest_substring(self, s: str) -> int:
+        def check(start, end):
+            chars = [0] * 128
+            for i in range(start, end + 1):
+                c = s[i]
+                chars[ord(c)] += 1
+                if chars[ord(c)] > 1:
+                    return False
+            return True
+
+        n = len(s)
+
+        res = 0
+        for i in range(n):
+            for j in range(i, n):
+                if check(i, j):
+                    res = max(res, j - i + 1)
+        return res
