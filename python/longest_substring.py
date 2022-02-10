02a7b2c0a7829a43d64633f1172cf47595e12877
Ewooral <boahen.cos@gmail.com>
2022-02-10 04:07:53 +0000

    longest substring without repeating characters

--- python/longest_substring.py
+++ python/longest_substring.py
@@ -66,3 +66,58 @@ class Solution:
                 if check(i, j):
                     res = max(res, j - i + 1)
         return res
+
+
+
+""" 
+Approach 2: Sliding Window
+Algorithm
+
+The naive approach is very straightforward. But it is too slow. So how can we optimize it?
+
+In the naive approaches, we repeatedly check a substring to see if it has duplicate character. But it is unnecessary. If a substring s_{ij}s 
+ij
+
+  from index ii to j - 1j−1 is already checked to have no duplicate characters. We only need to check if s[j]s[j] is already in the substring s_{ij}s 
+ij
+ .
+
+To check if a character is already in the substring, we can scan the substring, which leads to an O(n^2)O(n 
+2
+ ) algorithm. But we can do better.
+
+By using HashSet as a sliding window, checking if a character in the current can be done in O(1)O(1).
+
+A sliding window is an abstract concept commonly used in array/string problems. A window is a range of elements in the array/string which usually defined by the start and end indices, i.e. [i, j)[i,j) (left-closed, right-open). A sliding window is a window "slides" its two boundaries to the certain direction. For example, if we slide [i, j)[i,j) to the right by 11 element, then it becomes [i+1, j+1)[i+1,j+1) (left-closed, right-open).
+
+Back to our problem. We use HashSet to store the characters in current window [i, j)[i,j) (j = ij=i initially). Then we slide the index jj to the right. If it is not in the HashSet, we slide jj further. Doing so until s[j] is already in the HashSet. At this point, we found the maximum size of substrings without duplicate characters start with index ii. If we do this for all ii, we get our answer.
+
+
+Complexity Analysis
+
+Time complexity : O(2n) = O(n)O(2n)=O(n). In the worst case each character will be visited twice by ii and jj.
+
+Space complexity : O(min(m, n))O(min(m,n)). Same as the previous approach. We need O(k)O(k) space for the sliding window, where kk is the size of the Set. The size of the Set is upper bounded by the size of the string nn and the size of the charset/alphabet mm.
+"""
+
+
+class Solution:
+    def length_of_longest_substring_ii(self, s: str) -> int:
+        chars = [0] * 128
+
+        left = right = 0
+
+        res = 0
+        while right < len(s):
+            r = s[right]
+            chars[ord(r)] += 1
+
+            while chars[ord(r)] > 1:
+                l = s[left]
+                chars[ord(l)] -= 1
+                left += 1
+
+            res = max(res, right - left + 1)
+
+            right += 1
+        return res
