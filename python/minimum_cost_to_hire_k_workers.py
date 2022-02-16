a06950d467e88104fd7628dabe9d964ca243791c
Ewooral <boahen.cos@gmail.com>
2022-02-16 23:50:39 +0000

    minimum cost to hire K workers

--- python/minimum_cost_to_hire_k_workers.py
+++ python/minimum_cost_to_hire_k_workers.py
@@ -87,3 +87,32 @@ Time Complexity: O(N^2log N) where N is the number of workers.
 
 Space Complexity: O(N).
 """
+
+
+
+""" 
+Approach 2: Heap
+Intuition
+
+As in Approach #1, at least one worker is paid their minimum wage 
+expectation.
+
+Additionally, every worker has some minimum ratio of dollars to 
+quality that they demand. For example, if wage[0] = 100 and quality[0] = 20, 
+then the ratio for worker 0 is 5.0.
+
+The key insight is to iterate over the ratio. Let's say we hire 
+workers with a ratio R or lower. Then, we would want to know 
+the K workers with the lowest quality, and the sum of that quality. 
+We can use a heap to maintain these variables.
+
+Algorithm
+
+Maintain a max heap of quality. (We're using a minheap, with negative 
+values.) We'll also maintain sumq, the sum of this heap.
+
+For each worker in order of ratio, we know all currently considered 
+workers have lower ratio. (This worker will be the 'captain', as 
+described in Approach #1.) We calculate the candidate answer as this 
+ratio times the sum of the smallest K workers in quality.
+"""
