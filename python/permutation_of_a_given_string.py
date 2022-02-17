1ab32fe6eb781b356234ff48a1914cf32371f665
Ewooral <boahen.cos@gmail.com>
2022-02-17 03:52:20 +0000

    find permutation of a given array

--- python/permutation_of_a_given_string.py
+++ python/permutation_of_a_given_string.py
@@ -1,3 +1,43 @@
+""" 
+Given a string S. The task is to print all permutations of a given string in lexicographically sorted order.
+
+ 
+
+Example 1:
+
+Input: ABC
+Output:
+ABC ACB BAC BCA CAB CBA
+Explanation:
+Given string ABC has permutations in 6 
+forms as ABC, ACB, BAC, BCA, CAB and CBA .
+Example 2:
+
+Input: ABSG
+Output:
+ABGS ABSG AGBS AGSB ASBG ASGB BAGS 
+BASG BGAS BGSA BSAG BSGA GABS GASB 
+GBAS GBSA GSAB GSBA SABG SAGB SBAG 
+SBGA SGAB SGBA
+Explanation:
+Given string ABSG has 24 permutations.
+ 
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function find_permutaion() which takes the string S as input parameter and returns a vector of string in lexicographical order.
+
+ 
+
+Expected Time Complexity: O(n! * n)
+
+Expected Space Complexity: O(n)
+
+ 
+
+Constraints:
+1 <= length of string <= 5
+"""
+
 import itertools
 
 # T = O(n), S = O(n)
@@ -16,4 +56,5 @@ class Solution:
 Find_Permutation = Solution()
 print(Find_Permutation.find_permutation("ABSG"))
 print(len(Find_Permutation.find_permutation("ABSG")))
+print(len(Find_Permutation.find_permutation("STACK")))
 
