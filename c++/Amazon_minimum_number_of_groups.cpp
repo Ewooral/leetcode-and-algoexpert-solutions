d2bad1edeb5510e95b2b06a2c61284747afaf152
Ewooral <boahen.cos@gmail.com>
2022-02-18 21:43:27 +0000

    mininum number of groups -  Amazon SDE online assessment 2022

--- c++/Amazon_minimum_number_of_groups.cpp
+++ c++/Amazon_minimum_number_of_groups.cpp
@@ -34,7 +34,7 @@ function minGroupDifferK(arr, k)
 {
     arr.sort();
     let start = 0;
-    // if (arr.length == 0)
+    if (arr.length == 0)
     //     return 0;
     // // If arr has some value then at least can form 1 group
     // let count = 1;
