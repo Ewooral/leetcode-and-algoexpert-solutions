19fed0569c689c9bbef2915cc96023cd747882f5
Ewooral <boahen.cos@gmail.com>
2022-02-22 03:06:53 +0000

    remove duplicates from a linked list

--- python/remove_duplicate_linkedlist.py
+++ python/remove_duplicate_linkedlist.py
@@ -1,5 +1,15 @@
 """ 
 REMOVE DUPLICATES FROM LINKED LIST
 
-You're given the head
+You're given the head of a singly linked list whose nodes are in sorted order
+with respect to their values. Write a function that returns a modified version of the
+Linked list that doesn't contain any nodes with duplicate values.
+The linked list should be modified in place(i.e. you shouldn't create
+a brand new list), and the modified list should still have its nodes sorted with respect to
+their values. 
+
+Each Linked list node has an interger VALUE as well as a NODE pointer NEXT 
+pointing to the next node in the linked list or NULL/None if there are no more nodes.
+
+
 """
