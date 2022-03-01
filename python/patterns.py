fd4ee4bd28ac4b95f45f6b7596004fc09ab17d58
Ewooral <boahen.cos@gmail.com>
2022-03-01 12:35:41 +0000

    pattern in python

--- python/patterns.py
+++ python/patterns.py
@@ -1,8 +1,16 @@
 # Print a square pattern
 def squarePattern(size):
+    
+    for i in range(size):
+        # this loop will print 7 vertical rows
 
-    for j in range(1, size + 1):
-        print("#")
+        for j in range(0, size ):
+            # this loop will print 7 horizontal columns
+            print("#", end=" ")
+        print(".")
 
 
 squarePattern(7)
+
+
+def star_heart_shape(size):
