ddb498c3bab58fa59848783c926de06b3bff44be
Ewooral <boahen.cos@gmail.com>
2022-03-01 12:14:53 +0000

    pattern in python

---
+++ python/patterns.py
@@ -0,0 +1,8 @@
+# Print a square pattern
+def squarePattern(size):
+
+    for j in range(1, size + 1):
+        print("#")
+
+
+squarePattern(7)
--- python/stair_case.py
+++ python/stair_case.py
@@ -71,3 +71,5 @@ def staircases():
     print("######")
 staircases()
     
+
+
