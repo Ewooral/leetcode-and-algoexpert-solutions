8ed74d234bfd0b044d54bd58e3c750994c0be865
Ewooral <boahen.cos@gmail.com>
2022-03-04 13:12:32 +0000

    implementing the insertion sort algorithm

--- python/sorting/insertion_sort.py
+++ python/sorting/insertion_sort.py
@@ -3,7 +3,7 @@ def insertion_sort(arr):
         j = i
         while j > 0 and arr[j] < arr[j-1]: # while we are at the 2nd element of the array and the 
 #             # element is  out of position or the element is greater than the previous element
-#             arr[j-1], arr[j] = arr[j], arr[j-1]
+            arr[j-1], arr[j] = arr[j], arr[j-1]
 #             j -= 1
 #     return arr
     
