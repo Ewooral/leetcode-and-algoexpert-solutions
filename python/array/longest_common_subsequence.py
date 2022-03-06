92a096765f511b4e6be6551bc5b425d5cfe29a83
Ewooral <boahen.cos@gmail.com>
2022-03-06 21:41:54 +0000

    longest common subsequence

--- python/array/longest_common_subsequence.py
+++ python/array/longest_common_subsequence.py
@@ -62,7 +62,7 @@ class Solution:
             if text1[s1] == text2[s2]:
                 lookup[s1][s2] = 1 + self.dp(text1, text2, s1+1, s2+1, lookup)
             else:
-#                 lookup[s1][s2] = max(self.dp(
+                lookup[s1][s2] = max(self.dp(
 #                     text1, text2, s1+1, s2, lookup), self.dp(text1, text2, s1, s2+1, lookup))
 #         return lookup[s1][s2]
 
