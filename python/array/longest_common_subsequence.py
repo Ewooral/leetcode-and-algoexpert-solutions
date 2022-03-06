d8aec04e46c748d0793a9c40197e0a8d882f9df9
Ewooral <boahen.cos@gmail.com>
2022-03-06 21:43:43 +0000

    longest common subsequence

--- python/array/longest_common_subsequence.py
+++ python/array/longest_common_subsequence.py
@@ -81,7 +81,7 @@ class Solution:
             dp[i][0] = 0
         for j in range(len(text2)+1):
             dp[0][j] = 0
-#         for i in range(1, len(text1)+1):
+        for i in range(1, len(text1)+1):
 #             for j in range(1, len(text2)+1):
 #                 if text1[i-1] == text2[j-1]:
 #                     dp[i][j] = 1 + dp[i-1][j-1]
