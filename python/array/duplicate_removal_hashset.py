22732d290fde68338d62870f21b03ba75f51b688
Ewooral <boahen.cos@gmail.com>
2022-03-06 21:53:43 +0000

    duplicate_removal using hashset

--- python/array/duplicate_removal_hashset.py
+++ python/array/duplicate_removal_hashset.py
@@ -1,5 +1,5 @@
 def duplicate_removal(array):
-#     hashset = set()
+    hashset = set()
 #     for element in range(len(array)):
 #         if array[element] in hashset:
 #             return True, hashset
