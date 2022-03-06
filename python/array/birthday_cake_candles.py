88eb3afcd60e2398e86a5e4c570659ec234b79b8
Ewooral <boahen.cos@gmail.com>
2022-03-06 05:57:39 +0000

    buy and sell stock leetcode 121

--- python/array/birthday_cake_candles.py
+++ python/array/birthday_cake_candles.py
@@ -23,9 +23,26 @@ Input Format
 
 The first line contains a single integer, n, the size of candles.
 The second line contains  space-separated integers, where each integer  describes the height of candle[i] .
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+Explanation 0
+
+Candle heights are [3, 2, 1, 3]. The tallest candles are 3 units,
+and there are 2 of them.
 """
 
 
 def birthdayCakeCandles(candles):
     # Write your code here
     return candles.count(max(candles))
+
+
+if __name__ == '__main__':
+    print(birthdayCakeCandles([200, -234, 0, 2, 99, 1000, -18, 22, 1]))
+
