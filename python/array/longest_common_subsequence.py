885dc32aa9b394b31ca043ac2401ab600cec885f
Ewooral <boahen.cos@gmail.com>
2022-03-06 21:39:44 +0000

    longest common subsequence

--- python/array/longest_common_subsequence.py
+++ python/array/longest_common_subsequence.py
@@ -44,7 +44,7 @@ def longestCommonSubsequence(self, text1: str, text2: str) -> int:
             if text1[i] == text2[j]:
                 dp[i][j] = 1 + dp[i-1][j-1]
             else:
-#                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
 #             max_value = max(max_value, dp[i][j])
 
