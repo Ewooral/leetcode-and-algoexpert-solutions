2ffd55106cff083cafe5be4c3e1d35f3e7310a40
Ewooral <boahen.cos@gmail.com>
2022-03-07 21:48:49 +0000

    insertion sort

--- python/sorting/insertion_sort.py
+++ python/sorting/insertion_sort.py
@@ -1,14 +1,16 @@
 def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        j = i
-        while j > 0 and arr[j] < arr[j-1]: # while we are at the 2nd element of the array and the 
-#             # element is  out of position or the element is greater than the previous element
-            arr[j-1], arr[j] = arr[j], arr[j-1]
-            j -= 1
-    return arr
+#     for i in range(1, len(arr)):
+#         j = i
+#         while j > 0 and arr[j] < arr[j-1]: # while we are at the 2nd element of the array and the 
+# #             # element is  out of position or the element is greater than the previous element
+#             swap(j, j-1, arr)
+#             j -= 1
+#     return arr
+
+# def swap(i, j, arr):
+#     arr[i], arr[j] = arr[j], arr[i]
+
     
 
-
-   
-
-print(insertion_sort([5, 2, 4, 6, 1, 3]))
+# if __name__ == '__main__':
+#     print(insertion_sort([200, -234, 0, 2, 99, 1000, -18, 22, 1]))
