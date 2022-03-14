89700c447d8e89773e1b28c9b4ba99859df23055
Ewooral <boahen.cos@gmail.com>
2022-03-14 14:23:24 +0000

    frequency of a charater in a string

--- python/array/frequency_of_a_character.py
+++ python/array/frequency_of_a_character.py
@@ -46,7 +46,7 @@ def frequencySort(s):
         else:
             dic[i] = 1
 
-#     out = sorted(dic, key=dic.get, reverse=True)
+    out = sorted(dic, key=dic.get, reverse=True)
 
 #     for i in out:
 #         re += i*(dic[i])
