13420d62ff014888e338f04630c805c6b2dad3ee
Ewooral <boahen.cos@gmail.com>
2022-03-14 12:59:48 +0000

    frequency of a charater in a string

---
+++ python/array/frequency_of_a_character.py
@@ -0,0 +1,56 @@
+"""
+Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+
+ 
+
+Example 1:
+
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 105
+s consists of uppercase and lowercase English letters and digits.
+
+"""
+
+
+# def frequencySort(s):
+#     """
+#         :type s: str
+#         :rtype: str
+#         """
+#     dic = {}
+#     re = ""
+#     for i in s:
+#         if i in dic:
+#             dic[i] += 1
+#         else:
+#             dic[i] = 1
+
+#     out = sorted(dic, key=dic.get, reverse=True)
+
+#     for i in out:
+#         re += i*(dic[i])
+
+#     return re
+
+# print(frequencySort("tree"))
--- python/array/move_zeros.py
+++ python/array/move_zeros.py
@@ -8,16 +8,26 @@ def moveZeroes(arr):
     for index_of_zero_elements in range(count_nonzero_elements, len(arr)):
         arr[index_of_zero_elements] = 0
 
-    return arr
+    return arr, "No of non-zero elements: " + str(count_nonzero_elements)
 
 
 array = [0, 1, 0, 3, 12]
-print(moveZeroes(array))
+array_II = [0, 8, -2, 1, 0 ]
+array_III = [ 0]
+array_IV = [0, 4, -2, 0]
 
-""" Example:      0 1 0 3 12 
+print(moveZeroes(array)) 
+print(moveZeroes(array_II)) 
+print(moveZeroes(array_III)) 
+print(moveZeroes(array_IV)) 
 
-#  Expected Output: 1 3 12 0 0
+"""  
+ Question:
+ We are to push all zeros to the end of the array and 
+ maintain the order of the non zero elements in the array
 
-#  We are to push all zeros to the end of the array and 
-#  maintain the order of the non zero elements in the array
-# """
+    Example:      0, 1, 0, 3, 12 
+
+ Expected Output: 1, 3, 12, 0, 0
+ 
+"""
