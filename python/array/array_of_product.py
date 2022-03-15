3fe8a266911accfd5bc94a26879a6ace37092ac2
Ewooral <boahen.cos@gmail.com>
2022-03-15 19:53:30 +0000

    An optimised code for array of product, leetcode

--- python/array/array_of_product.py
+++ python/array/array_of_product.py
@@ -1,3 +1,4 @@
+# T = O(n^2), S = O(n)
 def array_of_product(array):
     # products = [1 for _ in range(len(array))];
     final_products = [None] * len(array);
@@ -12,9 +13,22 @@ def array_of_product(array):
 
 print(array_of_product([5, 1, 4, 2]))  # [8, 40, 10, 20] 
 
-
+# T = O(n), S = O(n)
 def array_of_product2(arr):
-    left, right, product = [1] * len(arr), [1] * len(arr), [1] * len(arr)
-    print(left, right, product)
+    leftProduct, rightProduct, product = [1] * len(arr), [1] * len(arr), [1] * len(arr)
+    # print(left, right, product)
+    left_product = 1   
+    for i in range(len(arr)):
+        leftProduct[i] = left_product
+        left_product *= arr[i]
+    right_product = 1
+    for i in range(len(arr)-1, -1, -1):
+        rightProduct[i] = right_product
+        right_product *= arr[i]
 
-array_of_product2([1,5,3,4,2])
+    for i in range(len(arr)):
+        product[i] = leftProduct[i] * rightProduct[i]
+    return product
+
+
+print(array_of_product2([5, 1, 4, 2]))  # [8, 40, 10, 20]
