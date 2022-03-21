83a04a6c426250a0655ed1fd4d66047136451fc8
Ewooral <boahen.cos@gmail.com>
2022-03-21 16:05:10 +0000

    recursive functions

--- python/recursion/fibonacci.py
+++ python/recursion/fibonacci.py
@@ -1,6 +1,6 @@
 def fibonacci(n):
-#     # n should be greater than or equals to 0 and a positive integer
-#     assert n >= 0 and int(n) == n, 'Only positive integers are accepted'
+    # n should be greater than or equals to 0 and a positive integer
+    assert n >= 0 and int(n) == n, 'Only positive integers are accepted'
 #     if n in [0, 1]:
 #         return n
 #     else:
