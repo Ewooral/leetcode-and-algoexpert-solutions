e454de37bf939ff7477e3874d2d292a622dd7739
Ewooral <boahen.cos@gmail.com>
2022-03-21 16:00:47 +0000

    recursive functions

--- python/recursion/power_of_two.py
+++ python/recursion/power_of_two.py
@@ -1,9 +1,9 @@
 def powerOfTwo(n):
-    if n == 0:
-        return 1
-    else:
-        # print(powerOfTwo(n-1) * 2);
-        return powerOfTwo(n-1) * 2
+#     if n == 0:
+#         return 1
+#     else:
+#         # print(powerOfTwo(n-1) * 2);
+#         return powerOfTwo(n-1) * 2
 
 
-print(powerOfTwo(4))
+# print(powerOfTwo(4))
