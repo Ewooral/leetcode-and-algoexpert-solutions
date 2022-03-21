559dd5d64736cc09821addecc74fc0827a61070f
Ewooral <boahen.cos@gmail.com>
2022-03-21 15:31:06 +0000

    recursion folder added

---
+++ python/recursion/sum_of_digits.py
@@ -0,0 +1,8 @@
+def Sum_of_digits(n):
+#     assert n >= 0 and int(n) == n, 'Only positive integers are accepted'
+#     if n == 0:
+#         return 0
+#     else:
+#         return int(n % 10) + Sum_of_digits(int(n/10));
+
+# print(Sum_of_digits(-1));
