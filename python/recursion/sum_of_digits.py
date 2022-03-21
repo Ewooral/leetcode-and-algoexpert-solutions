c2671ce660a3d5dde2fb3e94cbedd73f4a281e2c
Ewooral <boahen.cos@gmail.com>
2022-03-21 15:32:48 +0000

    recursive function - sum of digits

--- python/recursion/sum_of_digits.py
+++ python/recursion/sum_of_digits.py
@@ -2,7 +2,7 @@ def Sum_of_digits(n):
     assert n >= 0 and int(n) == n, 'Only positive integers are accepted'
     if n == 0:
         return 0
-#     else:
+    else:
 #         return int(n % 10) + Sum_of_digits(int(n/10));
 
 # print(Sum_of_digits(-1));
