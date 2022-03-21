2ce6095e299b962f8947f86dc436a560f1a86ee8
Ewooral <boahen.cos@gmail.com>
2022-03-21 15:59:15 +0000

    recursive functions

--- python/recursion/sum_of_digits.py
+++ python/recursion/sum_of_digits.py
@@ -1,5 +1,5 @@
 def Sum_of_digits(n):
-#     assert n >= 0 and int(n) == n, 'Only positive integers are accepted'
+    assert n >= 0 and int(n) == n, 'Only positive integers are accepted'
 #     if n == 0:
 #         return 0
 #     else:
