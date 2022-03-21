0b7271b22596c131649ba9d2a3b523bd7902cbc1
Ewooral <boahen.cos@gmail.com>
2022-03-21 16:02:51 +0000

    recursive functions

--- python/recursion/factorial.py
+++ python/recursion/factorial.py
@@ -1,6 +1,6 @@
 def factorial(n):
     assert n >= 0 and int(n) == n, 'Only positive integers are accepted'
-#     if n in [0, 1]:
+    if n in [0, 1]:
 #         return 1
 #     else:
 #         return n * factorial(n-1)
