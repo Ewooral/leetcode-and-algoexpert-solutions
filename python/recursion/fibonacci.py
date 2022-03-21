1e9bbac801b8884502bc77e8aba02b324c120a62
Ewooral <boahen.cos@gmail.com>
2022-03-21 16:05:35 +0000

    recursive functions

--- python/recursion/fibonacci.py
+++ python/recursion/fibonacci.py
@@ -4,7 +4,7 @@ def fibonacci(n):
     if n in [0, 1]:
         return n
     else:
-#         return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 # print(fibonacci(12))
