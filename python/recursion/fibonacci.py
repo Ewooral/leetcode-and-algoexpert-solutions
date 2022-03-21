f0180d8ae3e5d8e928384094491359798f101474
Ewooral <boahen.cos@gmail.com>
2022-03-21 16:05:24 +0000

    recursive functions

--- python/recursion/fibonacci.py
+++ python/recursion/fibonacci.py
@@ -2,7 +2,7 @@ def fibonacci(n):
     # n should be greater than or equals to 0 and a positive integer
     assert n >= 0 and int(n) == n, 'Only positive integers are accepted'
     if n in [0, 1]:
-#         return n
+        return n
 #     else:
 #         return fibonacci(n-1) + fibonacci(n-2)
 
