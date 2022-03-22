6d3e8e64520de9e6aad2f49583875890ad46d30b
Ewooral <boahen.cos@gmail.com>
2022-03-22 23:38:30 +0000

    implementing array

--- python/array/array_creation.py
+++ python/array/array_creation.py
@@ -37,7 +37,7 @@ traverse_array(arr1)
 
 # ACCESSING AN ELEMENT
 def access_element(array, index):
-#     if index >= len(array):
+    if index >= len(array):
 #         print("The element doesn't exit")
 #     else:
 #         print(array[index])
