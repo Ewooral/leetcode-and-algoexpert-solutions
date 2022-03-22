f9077885b5aa3da98dc42bc6f84766d4a12140c4
Ewooral <boahen.cos@gmail.com>
2022-03-22 21:41:41 +0000

    implementing array

--- python/array/tempCodeRunnerFile.py
+++ python/array/tempCodeRunnerFile.py
@@ -1,21 +1,4 @@
-def frequencySort(s):
-#     """
-#         :type s: str
-#         :rtype: str
-#         """
-    dic = {}
-    re = ""
-    for i in s:
-        if i in dic:
-            dic[i] += 1
-        else:
-            dic[i] = 1
 
-    out = sorted(dic, key=dic.get, reverse=True)
-
-    for i in out:
-        re += i*(dic[i])
-
-    return re
-
-print(frequencySort("tree"))
+# print(arr1);
+# print(arr2);
+# print(arr2[0]);
