3c3ac818d534b30e9dbabd312b3fe9cd14473c8e
Ewooral <boahen.cos@gmail.com>
2022-03-23 22:58:09 +0000

    creating two dimensional arrays

--- python/array/twoDArray.py
+++ python/array/twoDArray.py
@@ -64,10 +64,10 @@ def printElement(array, value):
     return None;
 
 # # O(m*n) and O(1) T and S complexity respectively
-# print(printElement(newtwoDarray, 9))
+print(printElement(newtwoDarray, 9))
 
 
-# print("...............................................");
+print("...............................................");
 
 # Deleting an item in a 2d array
 newtwoDarray = np.delete(twoDarray, 1, axis=0);
