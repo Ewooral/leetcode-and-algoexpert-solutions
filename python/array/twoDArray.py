f7f56ce59a0b646ed7eca1921dbc532666dadb28
Ewooral <boahen.cos@gmail.com>
2022-03-23 15:59:38 +0000

    creating two dimensional arrays

--- python/array/twoDArray.py
+++ python/array/twoDArray.py
@@ -3,7 +3,7 @@ twoDarray = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(twoDarray);  
 
 newtwoDarray = np.insert(twoDarray, 0, [[10, 11, 12]], axis=1)
-# print(newtwoDarray);
+print(newtwoDarray);
 
 # def printElement(array):
 #     for i in range(len(array)):
