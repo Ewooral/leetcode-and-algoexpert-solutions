82a85d0e91a2c2aaf58d9fb6f74c8ba0dc3705f5
Ewooral <boahen.cos@gmail.com>
2022-03-26 16:51:12 +0000

    binary search algorithm in PYTHON

--- python/searching/binary_search.py
+++ python/searching/binary_search.py
@@ -23,7 +23,7 @@ def isEven():
     number = binarySearch(array, item);
     if number is None: return -1;
 
-    if number % 2 == 0:
+    # if number % 2 == 0:
         return True; 
     return False;
 
