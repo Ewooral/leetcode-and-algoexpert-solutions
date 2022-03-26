10d2f16b793c714aadc16814a8b82a5c25aec988
Ewooral <boahen.cos@gmail.com>
2022-03-26 16:26:24 +0000

    binary search algorithm in PYTHON

--- python/searching/binary_search.py
+++ python/searching/binary_search.py
@@ -18,7 +18,7 @@ def binarySearch(array, item):
             low = mid + 1;
     return None;
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     array = [8, 2, 0, 1, 4, -2, 12];
     item = -22;
     array.sort();
