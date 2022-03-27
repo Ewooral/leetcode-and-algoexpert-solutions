76bffc8790058206ad127135525dfe6f54262120
Ewooral <boahen.cos@gmail.com>
2022-03-27 13:18:28 +0100

    finding the missing number in an array of numbers

--- python/array/missing_number.py
+++ python/array/missing_number.py
@@ -1,3 +1,11 @@
-myList = [[0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15,
-           16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]]
+# myList = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15,
+           16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
+# formula for finding the missing number of n series is n(n+1)/2 
+
+def find_missing_n_series(array, n):
+    sum1 = 30*31//2;
+    sum2 = sum(array);
+    print(sum1 - sum2);
+
+find_missing_n_series(myList, 31)
