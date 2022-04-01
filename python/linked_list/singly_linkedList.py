d34ce73205f03ee0a20153ef4c101940ca3e2a06
Ewooral <boahen.cos@gmail.com>
2022-04-01 14:36:32 +0100

    insertion at the beginning of a singly linked list

--- python/linked_list/singly_linkedList.py
+++ python/linked_list/singly_linkedList.py
@@ -33,23 +33,57 @@
 
 # INSERTING INTO A SINGLY LINKED LIST
 class Node:
-    def __init__(self, value=None):
+    def __init__(self, value = None):
         self.value = value
         self.next = None
 
 
-class SListList:
+class SLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+
+        # the below code makes our list printable
     def __iter__(self):
         node = self.head;
         while node:
             yield node;
             node = node.next
 
-    # insert in linked list
+# insert in linked list
     def insertSLL(self, value, location):
         newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None 
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next 
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
 
+
+
+singlyLinkedList = SLinkedList();
+singlyLinkedList.insertSLL(1, 1)
+singlyLinkedList.insertSLL(2, 1)
+singlyLinkedList.insertSLL(3, 1)
+singlyLinkedList.insertSLL(4, 1)  
+
+#insert at the beginning
+singlyLinkedList.insertSLL(30, 0)
+print([node.value for node in singlyLinkedList])
+    
 
