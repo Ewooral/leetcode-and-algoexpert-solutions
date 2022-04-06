960f093ee1af43dfe1b1ed081fef3eecde7055f5
Ewooral <boahen.cos@gmail.com>
2022-04-06 23:12:56 +0100

    circular singly linked list

--- python/linked_list/doubly_linked_list.py
+++ python/linked_list/doubly_linked_list.py
@@ -1,4 +1,7 @@
 
+from decimal import Inexact
+
+
 class Node:
     def __init__(self, value=None):
         self.value = value
@@ -28,9 +31,39 @@ class DoublyLinedList:
         return "Doubly linked list is created"
 
     def insertDLL(self, value, location):
-        
+    #     if self.head is None:
+    #         print("the node cannot be inserted")
+    #     else:
+    #         newNode = Node(value);
+    #         if location == 0:
+    #             newNode.previous = None;
+    #             newNode.next = self.head;
+    #             self.head.previous = newNode
+    #             self.head = newNode
+    #         elif location == 1:
+    #             newNode.next = None;
+    #             newNode.previous = self.tail
+    #             self.tail.next = newNode
+    #             self.tail = newNode
+    #         else:
+    #             tempNode = self.head
+    #             index = 0
+    #             while index < location - 1:
+    #                 tempNode = tempNode.next
+    #                 index += 1;
+    #             newNode.next = tempNode.next
+    #             newNode.previous = tempNode
+    #             newNode.next.previous = newNode
+    #             tempNode.next = newNode
+
+                
+
 
 doublyLL = DoublyLinedList()
 doublyLL.createDLL(5);
+doublyLL.insertDLL(1, 0)
+doublyLL.insertDLL(2, 1)
+doublyLL.insertDLL(3, 2)
+doublyLL.insertDLL(30, 3)
 
 print([node.value for node in doublyLL])
