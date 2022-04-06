f91e0eb79ea9c05ee7742daf5fae2027971c7cf5
Ewooral <boahen.cos@gmail.com>
2022-04-06 22:38:24 +0100

    circular singly linked list

---
+++ python/linked_list/doubly_linked_list.py
@@ -0,0 +1,33 @@
+
+class Node:
+#     def __init__(self, value=None):
+#         self.value = value
+#         self.next = None
+#         self.previous = None
+
+
+# class DoublyLinedList:
+#     def __init__(self) -> None:
+#         self.head = None
+#         self.tail = None
+
+#     def __iter__(self):
+#         node = self.head
+#         while node:
+#             yield node
+#             node = node.next
+            
+
+#     # O(1) T, O(1) S
+#     def createDLL(self, nodeValue=None):
+#         node = Node(nodeValue)
+#         node.next = None
+#         node.previous = None;
+#         self.head = node
+#         self.tail = node
+#         return "Doubly linked list is created"
+
+# doublyLL = DoublyLinedList()
+# doublyLL.createDLL(5);
+
+# print([node.value for node in doublyLL])
