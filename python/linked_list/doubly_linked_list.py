649ca00b3486938aeea64a92a6f3146a94534935
Ewooral <boahen.cos@gmail.com>
2022-04-06 22:38:52 +0100

    circular singly linked list

--- python/linked_list/doubly_linked_list.py
+++ python/linked_list/doubly_linked_list.py
@@ -3,7 +3,7 @@ class Node:
     def __init__(self, value=None):
         self.value = value
         self.next = None
-#         self.previous = None
+        self.previous = None
 
 
 # class DoublyLinedList:
