1068786ef0d4d59efb3ef99a040919432b3a02a8
Ewooral <boahen.cos@gmail.com>
2022-04-08 00:14:08 +0100

    circular doubly linked list

--- python/linked_list/circular_doublyLL.py
+++ python/linked_list/circular_doublyLL.py
@@ -1,96 +1,159 @@
-# #   Created by Boahen Owusu-Ewooral Elijah.
-# #   Copyright © 2022 Optimize Code. All rights reserved.
 
 class Node:
-    def __init__(self, value = None) -> None:
-        self.value = value;
-        self.next = None;
-        self.previous = None;
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+        self.prev = None
 
-class circularDoublyLinkedList:
-    def __init(self):
-        self.head = None;
-        self.tail = None;
+
+class CircularDoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
     def __iter__(self):
-        node = self.head;
+        node = self.head
         while node:
-            yield node;
-            node = node.next;
+            yield node
+            node = node.next
             if node == self.tail.next:
-                break;
-    def createCircularDLL(self, nodeValue):
-        node = Node(nodeValue);
-        self.head = node;
-        self.tail = node;
-        node.previous = node;
-        node.next = node
-        print("Circular doubly linked list has been successfully created!!"); 
+                break
 
-    def insertCircularDLL(self, value, location):
+    #  Creation of Circular Doubly Linked List
+    def createCDLL(self, nodeValue):
+        newNode = Node(nodeValue)
+        self.head = newNode
+        self.tail = newNode
+        newNode.prev = newNode
+        newNode.next = newNode
+        return "The CDLL is created successfully"
+
+    # Insertion Method in Circular Doubly Linked List
+
+    def insertCDLL(self, value, location):
         if self.head is None:
-            print("Nothing to insert because head is not available");
+            return "The CDLL does not exist"
         else:
-            node = Node(value);
+            newNode = Node(value)
             if location == 0:
-               node.next = self.head
-               node.previous = self.tail
-               self.head.previous = node
-               self.head = node;
-               self.tail.next = node
+                newNode.next = self.head
+                newNode.prev = self.tail
+                self.head.prev = newNode
+                self.head = newNode
+                self.tail.next = newNode
             elif location == 1:
-                node.next = self.head
-                node.previous = self.tail
-                self.head.previous = node
-                self.tail.next = node
-                self.tail = node;
+                newNode.next = self.head
+                newNode.prev = self.tail
+                self.head.prev = newNode
+                self.tail.next = newNode
+                self.tail = newNode
             else:
                 tempNode = self.head
                 index = 0
                 while index < location - 1:
                     tempNode = tempNode.next
-                    index += 1;
-                node.next = tempNode.next
-                node.prev = tempNode
-                node.next.prev = node
-                tempNode.next = node
+                    index += 1
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
             return "The node has been successfully inserted"
 
-
-
-    def traverseCircularDLL(self):
-          if self.head is None:
-              print("Head doesn'nt exit");
-          else:
-              node = self.head;
-              while node:
-                  print(node.value)
-                  node = node.next;
-                  if node == self.tail.next:
-                      break;
-    
-    def reverseTraverseCDLL(self):
+    # Traversal of Circular Doubly Linked List
+    def traversalCDLL(self):
         if self.head is None:
-            print("head doesn't exist");
+            print("There is not any node for traversal")
         else:
-            node = self.tail
-            while node:
-                print(node.value)
-                if node == self.head:
+            tempNode = self.head
+            while tempNode:
+                print(tempNode.value)
+                if tempNode == self.tail:
                     break
-                node = node.previous
-                
+                tempNode = tempNode.next
+
+    # Reverse traversal of Circular Doubly Linked List
+    def reverseTraversalCDLL(self):
+        if self.head is None:
+            print("There is not any node for reverse traversal")
+        else:
+            tempNode = self.tail
+            while tempNode:
+                print(tempNode.value)
+                if tempNode == self.head:
+                    break
+                tempNode = tempNode.prev
+
+    # Search Circular Doubly Linked List
+    def searchCDLL(self, nodeValue):
+        if self.head is None:
+            return "There is not any node in CDLL"
+        else:
+            tempNode = self.head
+            while tempNode:
+                if tempNode.value == nodeValue:
+                    return tempNode.value
+                if tempNode == self.tail:
+                    return "The value does not exist in CDLL"
+                tempNode = tempNode.next
+
+    # Delete a node from Circular Doubly Linked List
+    def deleteNode(self, location):
+        if self.head is None:
+            print("There is not any node to delete")
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head.prev = None
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+                    self.head.prev = self.tail
+                    self.tail.next = self.head
+            elif location == 1:
+                if self.head == self.tail:
+                    self.head.prev = None
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.tail = self.tail.prev
+                    self.tail.next = self.head
+                    self.head.prev = self.tail
+            else:
+                curNode = self.head
+                index = 0
+                while index < location - 1:
+                    curNode = curNode.next
+                    index += 1
+                curNode.next = curNode.next.next
+                curNode.next.prev = curNode
+            print("The node has been successfully deleted")
+
+    # Delete entire Circular Doubly Linked List
+    def deleteCDLL(self):
+        if self.head is None:
+            print("There is not any element to delete")
+        else:
+            self.tail.next = None
+            tempNode = self.head
+            while tempNode:
+                tempNode.prev = None
+                tempNode = tempNode.next
+            self.head = None
+            self.tail = None
+            print("The CDLL has been successfully deleted")
 
 
+circularDLL = CircularDoublyLinkedList()
+circularDLL.createCDLL(5)
+circularDLL.insertCDLL(0, 0)
+circularDLL.insertCDLL(1, 1)
+circularDLL.insertCDLL(2, 2)
 
-circularDLL = circularDoublyLinkedList();
-circularDLL.createCircularDLL(1);
-circularDLL.insertCircularDLL(2, 0);
-circularDLL.insertCircularDLL(3, 1);
-circularDLL.insertCircularDLL(30, 2);
-circularDLL.insertCircularDLL(4000, 3);
-
-print([node.value for node in circularDLL]);
-
-circularDLL.traverseCircularDLL()
-circularDLL.reverseTraverseCDLL()
+circularDLL.reverseTraversalCDLL()
+circularDLL.traversalCDLL()
+print([node.value for node in circularDLL])
+# circularDLL.deleteCDLL()
+# print([node.value for node in circularDLL])
