a13136c24f1e793cddc0de384dbb7507ac5ceb08
Ewooral <boahen.cos@gmail.com>
2022-04-07 23:55:34 +0100

    circular doubly linked list

--- python/linked_list/circular_doublyLL.py
+++ python/linked_list/circular_doublyLL.py
@@ -12,7 +12,7 @@ class circularDoublyLinkedList:
         self.head = None;
         self.tail = None;
 
-#     def __iter__(self):
+    def __iter__(self):
 #         node = self.head;
 #         while node:
 #             yield node;
