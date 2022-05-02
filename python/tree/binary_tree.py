a5d7d38c9abd5ff9c8c05a830f90fffd9190acad
Ewooral <boahen.cos@gmail.com>
2022-05-02 21:16:35 +0100

    binary tree

--- python/tree/binary_tree.py
+++ python/tree/binary_tree.py
@@ -49,7 +49,7 @@ def inOrderTraversal(rootNode):
         return
     inOrderTraversal(rootNode.leftChild)  # ........> O(n/2)
     print(rootNode.data)  # ........> O(1)
-    # inOrderTraversal(rootNode.rightChild)  # ........> O(n/2)
+    inOrderTraversal(rootNode.rightChild)  # ........> O(n/2)
 
 inOrderTraversal(newBT)
 print("...............postOrder...................")
