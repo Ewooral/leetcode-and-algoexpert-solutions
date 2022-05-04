0fb92872d1fd9fd140c4ecf405e6591b5f718e5e
Ewooral <boahen.cos@gmail.com>
2022-05-04 20:37:15 +0000

    binary tree data structure

--- python/tree/binarytree_module.py
+++ python/tree/binarytree_module.py
@@ -4,7 +4,7 @@ root = Node("Drink")
 
 root.left = Node("Hot")
 root.right = Node("Cold")
-# leftChild = root.left
+leftChild = root.left
 rightChild = root.right
 leftChild.left = Node("tea")
 leftChild.right = Node("coffee")
