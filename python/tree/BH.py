e62b014a6d8ecf175804fc875fde963b29032b86
Ewooral <boahen.cos@gmail.com>
2022-05-13 10:43:38 +0000

    creation of binary heap in python

--- python/tree/BH.py
+++ python/tree/BH.py
@@ -6,8 +6,25 @@ class BH:
     def __init__(self, size) -> None:
         self.List = (size + 1) * [None]
         self.heapSize = 0
-        self.maxSize = size
+        self.maxSize = size + 1
+
+
+# Peek of a BH is the root value
+# O(1)T, S
+def peek(rootNode):
+    if rootNode is None:
+        return
+    else:
+        return rootNode.List[1]
+
+# 
+def heapSize(rootNode):
+    if not rootNode:
+        return
+    else:
+        return rootNode.heapSize
 
 
 newBH = BH(5)
 print(newBH.List)
+print(heapSize(newBH))
