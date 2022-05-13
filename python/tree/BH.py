c868358131d1ba7987f70aab28413c1b5e0cbfb3
Ewooral <boahen.cos@gmail.com>
2022-05-13 13:17:10 +0000

    extracting and deleting  in a binary heap - python

--- python/tree/BH.py
+++ python/tree/BH.py
@@ -27,8 +27,14 @@ def heapSize(rootNode):
 # Traversal
 # O(N)T, O(1)S
 def levelorder(rootNode):
-    for i in range(1, rootNode.heapSize + 1):
-        print(rootNode.List[i])
+    try:
+        for i in range(1, rootNode.heapSize + 1):
+            print(rootNode.List[i])
+        
+    except (ValueError, RuntimeError, TypeError, NameError, SyntaxError) as err:
+        print(f"Oops!... traversing failed. List is empty")
+
+
 
 # O(log N)T, S
 def heapifyInsert(rootNode, index, heapType):
@@ -49,6 +55,7 @@ def heapifyInsert(rootNode, index, heapType):
             rootNode.List[parentIndex] = temp
         heapifyInsert(rootNode, parentIndex, heapType)
 
+# O(log N)T, S
 def insert(rootNode, nodeValue, heapType):
     if rootNode.heapSize + 1 == rootNode.maxSize:
         return "Binary Heap is full"
@@ -57,6 +64,66 @@ def insert(rootNode, nodeValue, heapType):
     heapifyInsert(rootNode, rootNode.heapSize, heapType)
     return "Inserted Successfully"
 
+# O(log N)T, S
+def heapifyExtract(rootNode, index, heapType):
+    leftIndex = index * 2
+    rightIndex = index * 2 + 1
+    swapChild = 0
+    if rootNode.heapSize < leftIndex:
+        return
+    elif rootNode.heapSize == leftIndex:
+        if heapType == "Min":
+            if rootNode.List[index] > rootNode.List[leftIndex]:
+                temp = rootNode.List[index]
+                rootNode.List[index] = rootNode.List[leftIndex]
+                rootNode.List[leftIndex] = temp
+            return 
+        else:
+            if rootNode.List[index] < rootNode.List[leftIndex]:
+                temp = rootNode.List[index]
+                rootNode.List[index] = rootNode.List[leftIndex]
+                rootNode.List[leftIndex] = temp
+            return 
+    else:
+        if heapType == "Min":
+            if rootNode.List[leftIndex] < rootNode.List[rightIndex]:
+                swapChild = leftIndex
+            else:
+                swapChild = rightIndex
+            if rootNode.List[index] > rootNode.List[swapChild]:
+                temp = rootNode.List[index]
+                rootNode.List[index] = rootNode.List[leftIndex]
+                rootNode.List[leftIndex] = temp
+        else:
+            if rootNode.List[leftIndex] > rootNode.List[rightIndex]:
+                swapChild = leftIndex
+            else:
+                swapChild = rightIndex
+            if rootNode.List[index] < rootNode.List[swapChild]:
+                temp = rootNode.List[index]
+                rootNode.List[index] = rootNode.List[leftIndex]
+                rootNode.List[leftIndex] = temp
+    heapifyExtract(rootNode, swapChild, heapSize)
+
+# The only element allowed to be extracted from a heap is the root Node
+# O(log N)T, S
+def extract(rootNode, heapType):
+    if rootNode.heapSize == 0:
+        return 
+    else:
+        extractedNode = rootNode.List[1]
+        rootNode.List[1] = rootNode.List[rootNode.heapSize]
+        rootNode.List[rootNode.heapSize] = None
+        rootNode.heapSize -= 1
+        heapifyExtract(rootNode, 1, heapType)
+        return extractedNode
+
+
+def deleteHeap(rootNode):
+    rootNode.List = None
+    return "Binary Heap Tree has been Deleted"
+
+            
 
 
 
@@ -67,6 +134,9 @@ insert(newBH, 5, "Max")
 insert(newBH, 2, "Max")
 insert(newBH, 1, "Max")
 levelorder(newBH)
+print("...extract Node...")
+extract(newBH, "Max")
+levelorder(newBH)
 print("....list....")
 print(newBH.List)
 print("...peek...")
@@ -75,4 +145,6 @@ print("...heap size...")
 print(heapSize(newBH))
 print("...traversal...")
 levelorder(newBH)
-
+print(".....delete binary heap tree...")
+deleteHeap(newBH)
+levelorder(newBH)
