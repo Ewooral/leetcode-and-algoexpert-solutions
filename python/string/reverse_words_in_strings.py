786b9bee6739b803cead10fc8965eff22bae2847
Ewooral <boahen.cos@gmail.com>
2022-06-01 17:41:36 +0000

    reverse words in a string

--- python/string/reverse_words_in_strings.py
+++ python/string/reverse_words_in_strings.py
@@ -25,7 +25,7 @@ def reverseList(list):
     start, end = 0, len(list) - 1;
     while start < end:
         list[start], list[end] = list[end], list[start];
-        start += 1;
+        # start += 1;
         end -= 1;
 
 print(reverseWordsInString("Hello Heavenly Father, I trust your works"));
