5c6e3c9265013d5adb002d8834d29e3369f772f6
Ewooral <boahen.cos@gmail.com>
2022-06-01 17:41:25 +0000

    reverse words in a string

--- python/string/reverse_words_in_strings.py
+++ python/string/reverse_words_in_strings.py
@@ -24,7 +24,7 @@ def reverseWordsInString(string):
 def reverseList(list):
     start, end = 0, len(list) - 1;
     while start < end:
-        list[start], list[end] = list[end], list[start];
+        # list[start], list[end] = list[end], list[start];
         start += 1;
         end -= 1;
 
