0162aae7312f390f229e3cabad9bbe7321e2ab2d
Ewooral <boahen.cos@gmail.com>
2022-06-04 23:53:43 +0000

    fractional knapsack problem - greedy algorithm

---
+++ python/Greedy/Fractional_Knapsack.py
@@ -0,0 +1,32 @@
+# class Item:
+    def __init__(self, weight, value):
+        self.weight = weight
+        self.value = value
+        self.ratio = value / weight
+
+
+def knapsackMethod(items, capacity):
+    items.sort(key=lambda x: x.ratio, reverse=True)
+    usedCapacity = 0
+    totalValue = 0
+    for i in items:
+        if usedCapacity + i.weight <= capacity:
+            usedCapacity += i.weight
+            totalValue += i.value
+        else:
+            unusedWeight = capacity - usedCapacity
+            value = i.ratio * unusedWeight
+            usedCapacity += unusedWeight
+            totalValue += value
+
+        if usedCapacity == capacity:
+            break
+    print("Total value obtained: "+str(totalValue))
+
+
+item1 = Item(20, 100)
+item2 = Item(30, 120)
+item3 = Item(10, 60)
+cList = [item1, item2, item3]
+
+knapsackMethod(cList, 50)
