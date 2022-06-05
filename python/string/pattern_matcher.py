38444b1f462691f9a23f5230564cc17e21ca837c
Ewooral <boahen.cos@gmail.com>
2022-06-05 22:07:56 +0000

    pattern matcher

--- python/string/pattern_matcher.py
+++ python/string/pattern_matcher.py
@@ -8,7 +8,7 @@ def pattern_matcher(pattern, string):
     if len(pattern) > len(string):
         return []
     newPattern = getNewPattern(pattern)
-    # didSwitch = newPattern[0] != pattern[0]
+    didSwitch = newPattern[0] != pattern[0]
     counts = {"X": 0, "y": 0}
     firstYpos = getCountsAndFirstYPos(newPattern, counts)
 
