7baa988834a08e02d62fa85a8846773fcd60214a
Ewooral <boahen.cos@gmail.com>
2022-06-05 22:07:42 +0000

    pattern matcher

--- python/string/pattern_matcher.py
+++ python/string/pattern_matcher.py
@@ -7,7 +7,7 @@ you're given two non empty strings.
 def pattern_matcher(pattern, string):
     if len(pattern) > len(string):
         return []
-    newPattern = getNewPattern(pattern)
+    # newPattern = getNewPattern(pattern)
     didSwitch = newPattern[0] != pattern[0]
     counts = {"X": 0, "y": 0}
     firstYpos = getCountsAndFirstYPos(newPattern, counts)
