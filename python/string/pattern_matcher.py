09ea924b0c819a5f7d8011bd1c30f2340e22c314
Ewooral <boahen.cos@gmail.com>
2022-06-05 22:07:19 +0000

    pattern matcher

--- python/string/pattern_matcher.py
+++ python/string/pattern_matcher.py
@@ -4,4 +4,11 @@ you're given two non empty strings.
 '''
 # O(n^2 + m) time | O(n + m) space
 
-# def pattern_matcher(pattern, string):
+def pattern_matcher(pattern, string):
+    # if len(pattern) > len(string):
+        return []
+    newPattern = getNewPattern(pattern)
+    didSwitch = newPattern[0] != pattern[0]
+    counts = {"X": 0, "y": 0}
+    firstYpos = getCountsAndFirstYPos(newPattern, counts)
+
