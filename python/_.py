acbf48dff62f7de8148bf5d8801d084f6a69a9d6
Ewooral <boahen.cos@gmail.com>
2022-06-08 16:57:49 +0000

    breadth first search recap

---
+++ python/_.py
@@ -0,0 +1,85 @@
+# two sum 
+'''
+array = [2, 5, 1, -1, 4, 3, 0]
+target = 3
+
+a + b = target
+a = currentValue
+b = nextValue
+
+hash_t = {
+    3 - 2=1: "Added",
+    3 - 5=-2: "Added",
+    3 - 1=2: "Added",
+    3 -(-1)=4: "Added",
+    3 - 4=-1: "not added",
+    
+    .
+    .
+    3 - 0: "Added",
+
+}
+
+'''
+
+
+def two_sum(clist: list, target: int) -> list:
+    hash_table = {}
+    for value in clist:
+        nextValue = target - value
+        if nextValue in hash_table:
+            print([value, nextValue])
+        else:
+            hash_table[value] = "Added"
+    print(hash_table)
+    return []
+
+
+
+
+'''
+[1, 3, 9, 1, 0]
+'''
+from collections import deque, defaultdict
+# from typing import Any
+
+Queues = deque()
+Queues.append(2)
+Queues.append(20)
+Queues.append(22)
+
+
+
+class Graph:
+    def __init__(self, graph = None) -> None:
+        if graph is None:
+            graph = defaultdict()
+        self.graph = graph
+
+    def add_edge(self, vertex, edges) -> None:
+        self.graph[vertex].append(edges)
+
+
+
+
+
+def main() -> None:
+    # queue
+    print(Queues)
+    Queues.popleft()
+    print(Queues)
+
+    # two sum problem
+    print("............")
+    two_sum([2, 5, 1, -1, 4, 3, 0], 3)
+
+    print("............")
+    # graph
+    graph = Graph()
+    print(graph.graph.get("Elijah"))
+    print(graph.add_edge("Kumasi", 4))
+
+
+if __name__ == "__main__":
+    main()
+
