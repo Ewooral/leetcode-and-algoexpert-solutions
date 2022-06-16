220a046be1e624f3876bd7baafd069f49fa424fd
Ewooral <boahen.cos@gmail.com>
2022-06-16 23:44:19 +0000

    remove duplicate from linkedlist-AlgoExpert Question

--- python/linked_list/AlgoExpert questions and answers/remove_duplicates_in_linkedlist.py
+++ python/linked_list/AlgoExpert questions and answers/remove_duplicates_in_linkedlist.py
@@ -39,7 +39,7 @@ def main():
     print(".............")
     remove_duplicates(linkedlist)
     linkedlist.print_list()
-    # print(linkedlist.value)
+    print(linkedlist.value)
 
 if __name__ == '__main__':
     main()
