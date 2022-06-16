42fc6d2b98f4a0dc91b955c11cf381b72eeeef3b
Ewooral <boahen.cos@gmail.com>
2022-06-16 23:35:29 +0000

    remove duplicate from linkedlist-AlgoExpert Question

---
+++ python/linked_list/AlgoExpert questions and answers/remove_duplicates_in_linkedlist.py
@@ -0,0 +1,45 @@
+# from dataclasses import dataclass
+from typing import Any, Optional
+
+
+@dataclass
+class LinkedList:
+    value: Any = None
+    next: Optional[Any] = None
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+
+
+def remove_duplicates(linkedList):
+    currentNode = linkedList
+    while currentNode is not None:
+        nextDistinctNode = currentNode.next
+        while nextDistinctNode is not None and nextDistinctNode.value == currentNode.value:
+            nextDistinctNode = nextDistinctNode.next 
+        
+        currentNode.next = nextDistinctNode
+        currentNode = nextDistinctNode
+    return LinkedList
+
+def main():
+    linkedlist = LinkedList(value=1)
+    linkedlist.next = LinkedList(value= 2)
+    linkedlist.next.next = LinkedList(value= 2)
+    linkedlist.next.next.next = LinkedList(value= 3)
+    linkedlist.next.next.next.next = LinkedList(value= 3)
+    linkedlist.next.next.next.next.next = LinkedList(value= 4)
+    print()
+    print()
+    print(".............")
+    remove_duplicates(linkedlist)
+    linkedlist.print_list()
+    print(linkedlist.value)
+
+if __name__ == '__main__':
+    main()
