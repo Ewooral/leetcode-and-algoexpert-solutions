026e8ddd7daf1e19181646b332a41733ee0e5db9
Ewooral <boahen.cos@gmail.com>
2022-06-17 23:48:46 +0000

    doubly linkedlist construction and several operations performed on it

--- python/linked_list/AlgoExpert questions and answers/linked_list_construction.py
+++ python/linked_list/AlgoExpert questions and answers/linked_list_construction.py
@@ -35,7 +35,7 @@ class DoublyLinkedList:
     def setHead(self, node):
         if self.head is None:
             self.head = node
-            self.tail = node
+            # self.tail = node
             return
         self.insertBefore(self.head, node)
 
