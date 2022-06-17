2e237421dd3edbc7474dc38ce6be29cdf899656a
Ewooral <boahen.cos@gmail.com>
2022-06-17 00:42:25 +0000

    fizzbuzz algorithm javascript

--- java/MaxSumSubarray.java
+++ java/MaxSumSubarray.java
@@ -1,10 +1,8 @@
 public class MaxSumSubarray{
-    public static void main(String[] args){
-        System.out.println(findMaxSumSubarray(new int[]{ 4, 2, 1, 7, 8, 1, 2, 8, 1, 2, 8, 1, 0}, 3));
-    }
+  
 
-    static int findMaxSumSubarray(int[] arr, int k){
-        int maxValue = Integer.MAX_VALUE;
+    public static int findMaxSumSubarray(int[] arr, int k){
+        int maxValue = Integer.MIN_VALUE;
         int currentRunningSum = 0;
 
         for(int i = 0; i < arr.length; i++){
@@ -16,5 +14,9 @@ public class MaxSumSubarray{
         }
 
         return maxValue;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMaxSumSubarray(new int[] { 4, 2, 1, 7, 8, 1, 2, 8, 1, 0 }, 3));
     }
 }
--- javascript/fizzbuzz_algo.js
+++ javascript/fizzbuzz_algo.js
@@ -1,5 +1,6 @@
 // Challenge: fizzbuzz
-// Create a function fizzbuzz that takes one number, n. fizzbuzz should loop through the numbers 1 through n and push each number into the results array using the following rules:
+// Create a function fizzbuzz that takes one number, n. fizzbuzz should loop through
+//  the numbers 1 through n and push each number into the results array using the following rules:
 // 1. Push the string "fizz" in place of numbers divisible by 3.
 // 2. Push the string "buzz" in place of numbers divisible by 5.
 // 3. Push the string "fizzbuzz" in place of numbers divisible by both 3 and 5.
@@ -32,7 +33,7 @@ console.log(results);
 // should log: [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16]
 
 
-console.log(global.process.env.path.length)
+// console.log(global.process.env.path.length)
 
 
 (function isEvening(){
