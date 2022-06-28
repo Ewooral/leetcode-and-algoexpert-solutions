2dc775408ba1e6f6fea6b490103b28594df606a1
Ewooral <boahen.cos@gmail.com>
2022-06-28 23:54:40 +0000

    branch sums - binary tree data structure interview question

--- python/tree/questions and answers/branch_sums.py
+++ python/tree/questions and answers/branch_sums.py
@@ -21,7 +21,7 @@ def insertAtRight(rootNode, nodeValue):
     if rootNode is None:
         rootNode = nodeValue
     else:
-        q = queue.Queue()
+        # q = queue.Queue()
         q.put(rootNode)
         while not q.empty():
             root = q.get()
