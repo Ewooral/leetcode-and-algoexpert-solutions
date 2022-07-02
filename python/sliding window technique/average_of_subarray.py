7dea7a7e1e4a646e0862e0c9809dbbc1e97070b4
Ewooral <boahen.cos@gmail.com>
2022-07-02 23:15:52 +0000

    max sum of subarray - sliding window technique

--- python/sliding window technique/average_of_subarray.py
+++ python/sliding window technique/average_of_subarray.py
@@ -10,9 +10,30 @@ def find_averages_of_subarrays(K, arr):
   return result
 
 
+def find_averages_of_subarrays_1(K, arr):
+  result = []
+  windowSum = 0.0
+  windowStart = 0
+  for windowEnd in range(len(arr)):
+    windowSum += arr[windowEnd]  # add the next element
+    # slide the window, we don't need to slide if we've not hit the required window size of 'k'
+    if windowEnd >= K - 1:
+      result.append(windowSum / K)  # calculate the average
+      windowSum -= arr[windowStart]  # subtract the element going out
+      windowStart += 1  # slide the window ahead
+
+  return result
+
+
 def main():
   result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
+  result1 = find_averages_of_subarrays_1(5, [2, 1, 0, 8, 4, 3, 9])
   print("Averages of subarrays of size K: " + str(result))
+  print("Averages of subarrays of size K: " + str(result1))
 
 
 main()
+
+
+
+
