94d289c1c6edbfb7a7eb01adc2985cdad0017afb
Ewooral <boahen.cos@gmail.com>
2022-07-08 16:32:49 +0000

    first non repeating character - python

--- python/Graph/SSSP.py
+++ python/Graph/SSSP.py
@@ -21,22 +21,6 @@ class Graph:
                 queue.append(new_path)
 
 
-def find_path(graph, start, end, path=None):
-    # print("find_path", "(", path, ")")
-    if path is None:
-        path = []
-    path += [start]
-    if start == end:
-        return path
-    for node in graph[start]:
-        if node not in path:
-            new_path = find_path(graph, node, end, path)
-            if len(new_path) > 0:
-                return new_path
-        # return "Cycle has been detected!"
-    return None
-
-
 customDict = {"a": ["b", "c"],
               "b": ["d", "g"],
               "c": ["d", "e"],
@@ -58,5 +42,3 @@ graph1 = Graph(ano)
 
 print(graph1.BFS("a", "f"))
 print(graph1.BFS("a", "e"))
-# print(find_path(graph.graphDic, "a", "e"))
-# print(find_path(graph.graphDic, "a", "f"))
---
+++ python/string/first_non_repeating_char.py
@@ -0,0 +1,28 @@
+# O(N^2)T, O(1)S
+def first_non_repeating_char(string):
+    found_duplicate = False
+    for ch in range(len(string)):
+        for ch2 in range(len(string)):
+            if string[ch] == string[ch2] and ch != ch2:
+                found_duplicate = True
+        if not found_duplicate:
+            return string[ch]
+    return -1
+
+
+print(first_non_repeating_char("abcdfa"))
+
+
+# O(N)T, O(1)S
+def FirstNonRepeatingChar(Str):
+    freq = {}
+    for ch in Str:
+        freq[ch] = freq.get(ch, 0) + 1
+    for i in range(len(Str)):
+        ch2 = Str[i]
+        if freq[ch2] == 1:
+            return ch2
+    return -1
+
+
+print(first_non_repeating_char("abcdfa"))
