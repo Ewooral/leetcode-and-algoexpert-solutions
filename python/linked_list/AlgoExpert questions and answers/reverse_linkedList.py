fb7cf9444c57d459c5170990a760e04e504d5c1f
Ewooral <boahen.cos@gmail.com>
2022-07-10 23:56:26 +0000

    reverse a linkedlist - algoexpert question

--- python/linked_list/AlgoExpert questions and answers/reverse_linkedList.py
+++ python/linked_list/AlgoExpert questions and answers/reverse_linkedList.py
@@ -2,7 +2,7 @@
 def reverseLinkedList(head):
     p1, p2 = None, head
     while p2 is not None:
-        # p3 = p2.next
+        p3 = p2.next
         p2.next = p1
         p1 = p2
         p2 = p3
