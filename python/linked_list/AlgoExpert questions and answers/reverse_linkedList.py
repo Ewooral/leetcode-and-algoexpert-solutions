63ada1e52a618a2ba76a070cf45964b1703ad820
Ewooral <boahen.cos@gmail.com>
2022-07-11 23:49:58 +0000

    reverse a linked list

--- python/linked_list/AlgoExpert questions and answers/reverse_linkedList.py
+++ python/linked_list/AlgoExpert questions and answers/reverse_linkedList.py
@@ -24,7 +24,7 @@ class Node:
 def reverse(head):
     previous, current, next = None, head, None
     while current is not None:
-        # next = current.next  # temporarily store the next node
+        next = current.next  # temporarily store the next node
         current.next = previous  # reverse the current node
         previous = current  # before we move to the next node, point previous to the current node
         current = next  # move on the next node
