0da6f8478f2edae19614b652d46693ec7fa3dfa5
Ewooral <boahen.cos@gmail.com>
2022-07-11 23:47:35 +0000

    reverse a linked list

--- python/linked_list/AlgoExpert questions and answers/reverse_linkedList.py
+++ python/linked_list/AlgoExpert questions and answers/reverse_linkedList.py
@@ -11,7 +11,7 @@ from __future__ import print_function
 class Node:
     def __init__(self, value, next=None):
         self.value = value
-        # self.next = next
+        self.next = next
 
     def print_list(self):
         temp = self
