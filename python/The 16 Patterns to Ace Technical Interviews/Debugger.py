3ae588f0b52723f076c91b3318e6bb68b6fce3eb
Ewooral <boahen.cos@gmail.com>
2022-08-06 23:47:44 +0000

    string splosion python

---
+++ python/The 16 Patterns to Ace Technical Interviews/Debugger.py
@@ -0,0 +1,36 @@
+def fourSum(nums, target):
+    nums.sort()
+    return kSum(nums, target, 5)
+
+def kSum(nums, target, k):
+        res = []
+        if not nums:
+            return res
+        average_value = target // k
+        if average_value < nums[0] or nums[-1] < average_value:
+            return res
+        if k == 2:
+            return twoSum(nums, target)
+        for i in range(len(nums)):
+            if i == 0 or nums[i - 1] != nums[i]:
+                for subset in kSum(nums[i + 1:], target - nums[i], k - 1):
+                    res.append([nums[i]] + subset)
+        return res
+
+
+def twoSum(nums, target):
+        res = []
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            curr_sum = nums[lo] + nums[hi]
+            if curr_sum < target or (lo > 0 and nums[lo] == nums[lo - 1]):
+                lo += 1
+            elif curr_sum > target or (hi < len(nums) - 1 and nums[hi] == nums[hi + 1]):
+                hi -= 1
+            else:
+                res.append([nums[lo], nums[hi]])
+                lo += 1
+                hi -= 1
+        return res
+
+print("Four sum: ", fourSum([3, 2, 0, -1, 4, 2, -2, 1, 9, -4], 2))
