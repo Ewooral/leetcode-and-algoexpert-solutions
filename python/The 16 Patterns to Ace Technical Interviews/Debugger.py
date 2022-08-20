1ea426f6e4edf045930aee1816ebb13ac91a38ae
Ewooral <boahen.cos@gmail.com>
2022-08-20 18:08:41 +0000

    walrus operator in python

--- python/The 16 Patterns to Ace Technical Interviews/Debugger.py
+++ python/The 16 Patterns to Ace Technical Interviews/Debugger.py
@@ -1,22 +1,17 @@
-def selection_sort(arr):
-    for i in range(len(arr)):
-        minIdx = i
-        for j in range(i+1, len(arr)):
-            if arr[minIdx] > arr[i]:
-                minIdx = j
-        swap(i, minIdx, arr)
-
-    return arr
-
-
-def swap(i, j, arr):
-    arr[i], arr[j] = arr[j], arr[i]
+def sort(arr: list):
+    res = []
+    i = 0
+    while len(arr) > 0:
+        mini = min(arr)
+        res.insert(i, mini)
+        arr.remove(mini)
+        i += 1
+    return res
 
 
 def main():
-    # arr = [-5, -2, 1, 3]
-    # arr1 = [8, -2, 1, 3]
-    arr2 = [8, 7, 2, 1, 0, 9, 6]
-    print(selection_sort(arr2))
+    arr = [8, -2, 1, 3]
+    sort(arr)
+
 
 main()
