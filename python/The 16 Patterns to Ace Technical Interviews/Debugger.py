7790004414a43cb86455a2bbc53da3ad3a55f4b3
Ewooral <boahen.cos@gmail.com>
2022-08-21 23:45:09 +0000

    Merge sort in python

--- python/The 16 Patterns to Ace Technical Interviews/Debugger.py
+++ python/The 16 Patterns to Ace Technical Interviews/Debugger.py
@@ -1,24 +1,15 @@
-def Bubble_Sort(array):
-    isSorted = False
-    # counter keeps track of the sorted region on the right and avoid
-    # looping through that region since it is already sorted
-    counter = 0
-    while not isSorted:
-        isSorted = True
-        for i in range(len(array) - 1 - counter):
-            if array[i] > array[i + 1]:
-                swap1(i, i + 1, array)
-                isSorted = False
-        counter += 1
-    return array
+def findContentChildren( g, s):
+    g = sorted(g)
+    s = sorted(s)
+    ans, i, j = 0, 0, 0
+    while i < len(s) and j < len(g):
+        if s[i] >= g[i]:
+            ans += 1
+            i += 1
+            j += 1
+        else:
+            i += 1
+    return ans
 
-def swap1(i, j, array):
-    array[i], array[j] = array[j], array[i]
-
-
-def main():
-    arr = [8, -2, 1, 3]
-    print(Bubble_Sort(arr))
-    Bubble_sort([9, 2, 3, 8, 10, 1, 4])
-
-main()
+print(findContentChildren([1,2,3], [1,1]))
+print(findContentChildren([1,2], [1,2,3]))
