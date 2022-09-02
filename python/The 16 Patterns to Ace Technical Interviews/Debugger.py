8b5f81ef595113316e767e2081dafa7bfbc98b49
Ewooral <boahen.cos@gmail.com>
2022-09-02 23:48:25 +0000

    top k element pattern in interview questions

--- python/The 16 Patterns to Ace Technical Interviews/Debugger.py
+++ python/The 16 Patterns to Ace Technical Interviews/Debugger.py
@@ -6,14 +6,17 @@ b = [16, 20, 90]
 def pbB(p, b):
     i = j = 0
     res, lengthP, lengthB = [], len(p), len(b)
+    beg= b[j]
     while j < lengthB:
         count = 0
+        poi, bou, nex = p[i], b[j], b[j+1]
+        while p[i] < b[j] and p[i] < b[j+1]:
+            i += 1
         while i < lengthP and b[j] < p[i] < b[j + 1]:
             count += 1
             i += 1
-        while i < lengthP and p[i] < b[j]:
-            count += 1
-            i += 1
+        while i < lengthP and p[i] < b[j] and p[i] < b[j + 1]:
+            j += 1
         if not count == 0:
             res.append(count)
         j += 1
