359f941002736b12401e4562c870884e785317af
Ewooral <boahen.cos@gmail.com>
2022-09-03 23:55:41 +0000

    top kth element leetcode coding pattern

--- python/The 16 Patterns to Ace Technical Interviews/Debugger.py
+++ python/The 16 Patterns to Ace Technical Interviews/Debugger.py
@@ -1,26 +1,13 @@
-points = [2.3, 3.5, 4.4, 7.8]
-boundary = [0, 5, 10]
-p = [81, 15, 4, 12, 1]
-b = [16, 20, 90]
+import _heapq as heap
+from typing import List
 
-def pbB(p, b):
-    i = j = 0
-    res, lengthP, lengthB = [], len(p), len(b)
-    beg= b[j]
-    while j < lengthB:
-        count = 0
-        poi, bou, nex = p[i], b[j], b[j+1]
-        while p[i] < b[j] and p[i] < b[j+1]:
-            i += 1
-        while i < lengthP and b[j] < p[i] < b[j + 1]:
-            count += 1
-            i += 1
-        while i < lengthP and p[i] < b[j] and p[i] < b[j + 1]:
-            j += 1
-        if not count == 0:
-            res.append(count)
-        j += 1
+def findkthLargest(nums:List[int], k:int):
+    minHeap = []
+    for num in nums:
+        heap.heappush(minHeap, num)
+        if len(minHeap) > k:
+            heap.heappop(minHeap)
+    return min(minHeap)
 
-    return res
-
-pbB(p, b)
+print(findkthLargest([3, 2, 1, 5, 6, 4], 2))
+print(findkthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
