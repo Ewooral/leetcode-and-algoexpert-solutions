8d620c41fcc2fb8ec29a31a99b3d0817feb31b5e
Ewooral <boahen.cos@gmail.com>
2022-09-23 23:59:00 +0000

    fix: Bubble sort algorithm

--- python/The 16 Patterns to Ace Technical Interviews/Debugger.py
+++ python/The 16 Patterns to Ace Technical Interviews/Debugger.py
@@ -1,26 +1,26 @@
-import heapq as heap
+# INSERTION SORT
+"""
+1. First Item in the list is always deemed as sorted.
+2. Traverse each item in the list starting from the second item and find the minimum
+3. Compare the item to the sorted item. If sorted item is lesser, do nothing, else, swap both items.
+
+kye = [9, 2, 3, 8, 10, 1, 4]
+      [2, 3, 8, 9, 1, 4, 10]
+
+i = 0
+j = 0
+"""
+
+lst = [2, 0, 1, 7, -12]
+kye = [9, 2, 3, 8, 10, 1, 4]
 
 
-def find_Kth_smallest_number(nums, k):
-  # TODO: Write your code here
-  result = []
-  for num in nums:
-      heap.heappush(result,num)
-  return result[k-1]
+def Bubble_sort(array):
+    for i in range(len(array) - 1):
+        for j in range(len(array) - 1):
+            if array[j] > array[j+1]:
+                array[j+1], array[j] = array[j], array[j+1]
+    print("Approach 1: ", array)
 
 
-
-def main():
-
-  # print("Kth smallest number is: " +
-        # str(find_Kth_smallest_number([1, 5, 12, 2, 11, 5], 3)))
-
-  # since there are two 5s in the input array, our 3rd and 4th smallest numbers should be a '5'
-  # print("Kth smallest number is: " +
-  #       str(find_Kth_smallest_number([1, 5, 12, 2, 11, 5], 4)))
-
-  print("Kth smallest number is: " +
-        str(find_Kth_smallest_number([5, 12, 11, -1, 12], 3)))
-
-
-main()
+print(Bubble_sort(lst))
